3d1f27f444f236714edbf2349ad4f242f055db14
sikgyu <dlatlrrb@gmail.com>
2021-10-28 10:12:03 -0700

    Create Audit_log folder

---
+++ Audit/app.py
@@ -0,0 +1,82 @@
+import connexion
+from pykafka import KafkaClient
+from flask import Flask
+from flask_cors import CORS
+
+import logging.config
+import yaml
+import json
+
+with open('app_conf.yml', 'r') as f:
+    app_config = yaml.safe_load(f.read())
+
+with open('log_conf.yml', 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+
+logger = logging.getLogger('basicLogger')
+
+logger.info("connecting to DB. Hostname: %s, Port: %d" % (app_config["datastore"]["hostname"],
+                                                          app_config["datastore"]["port"]))
+
+
+def get_final_grade_event(index):
+    hostname = "%s:%d" % (app_config["events"]["hostname"],
+                          app_config["events"]["port"])
+    client = KafkaClient(hosts=hostname)
+    topic = client.topics[str.encode(app_config["events"]["topic"])]
+
+    consumer = topic.get_simple_consumer(reset_offset_on_start=True,
+                                         consumer_timeout_ms=1000)
+    logger.info("Retrieving final grade at index %d" % index)
+
+    count = 0
+    try:
+        for msg in consumer:
+            msg_str = msg.value.decode('utf-8')
+            msg = json.loads(msg_str)
+            if msg["type"] == "final_grade":
+                if count == index:
+                    event = msg["payload"]
+                    return event, 200
+                count += 1
+    except:
+        logger.error("No more messages found")
+
+    logger.error("Could not find final grade at index %d" % index)
+    return {"message": "Not Found"}, 404
+
+
+def get_classes_event(index):
+    hostname = "%s:%d" % (app_config["events"]["hostname"],
+                          app_config["events"]["port"])
+    client = KafkaClient(hosts=hostname)
+    topic = client.topics[str.encode(app_config["events"]["topic"])]
+
+    consumer = topic.get_simple_consumer(reset_offset_on_start=True,
+                                         consumer_timeout_ms=1000)
+    logger.info("Retrieving classes at index %d" % index)
+
+    count = 0
+    try:
+        for msg in consumer:
+            msg_str = msg.value.decode('utf-8')
+            msg = json.loads(msg_str)
+            if msg["type"] == "classes":
+                if count == index:
+                    event = msg["payload"]
+                    return event, 200
+                count += 1
+    except:
+        logger.error("No more messages found")
+
+    logger.error("Could not find classes at index %d" % index)
+    return {"message": "Not Found"}, 404
+
+
+app = connexion.FlaskApp(__name__, specification_dir='')
+app.add_api("openapi.yaml",
+            strict_validation=True,
+            validate_responses=True)
+if __name__ == "__main__":
+    app.run(port=8110)
