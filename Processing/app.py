00782242c8888a78828146ffa0f360219c8154a3
sikgyu <57619483+sikgyu@users.noreply.github.com>
2021-12-01 20:29:21 -0800

    .

--- Processing/app.py
+++ Processing/app.py
@@ -126,15 +126,13 @@ def init_scheduler():
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yaml",
-            base_path="/processing",
-            strict_validation=True,
-            validate_responses=True)
+app.add_api("openapi.yaml", base_path="/processing", strict_validation=True, validate_responses=True)
 
 if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
     CORS(app.app)
     app.app.config['CORS_HEADERS']='Content-Type'
-    
+
+
 if __name__ == "__main__":
     init_scheduler()
     app.run(port=8100, use_reloader=False)
--- Receiver/app.py
+++ Receiver/app.py
@@ -99,10 +99,7 @@ def instructor_account(body):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yaml",
-            base_path="/receiver",  
-            strict_validation=True,
-            validate_responses=True)
+app.add_api("openapi.yaml", base_path="/receiver", strict_validation=True, validate_responses=True)
 
 
 if __name__ == "__main__":
